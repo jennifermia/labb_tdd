6b4837e7671337b680c78d591ee6ea9c80cfa37f
jennifermia <jennifer.toivanen@iths.se>
2022-02-15 14:43:38 +0100

    Refactor

--- src/test/java/EmployeeTest.java
+++ src/test/java/EmployeeTest.java
@@ -1,10 +1,17 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 public class EmployeeTest {
+    Employee testObject;
 
     @Test
     public void EmployeeConstructor(){
-        Employee testObject = new Employee();
+
+
+
+
     }
 
 }
