06d73adff98e03246c4cd39510883b933c5ba4dd
jennifermia <jennifer.toivanen@iths.se>
2022-02-16 11:55:21 +0100

    green

--- src/test/java/EmployeeSystem.java
+++ src/test/java/EmployeeSystem.java
@@ -1,6 +1,12 @@
+import java.util.ArrayList;
+
 public class EmployeeSystem {
 
     public void addEmployee(Employee tjomme) {
 
+    }
+
+    public ArrayList<Employee> getEmployeeList() {
+        return null;
     }
 }
