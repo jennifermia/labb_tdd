f8b9386ef8faf1537ac8e7ae2ca4c4d28ad21820
jennifermia <jennifer.toivanen@iths.se>
2022-02-16 11:27:46 +0100

    Red

---
+++ src/test/java/EmployeeSystemTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+public class EmployeeSystemTest {
+
+    @Test
+    public void test_AddEmployee(){
+        EmployeeSystem testObject = new EmplyeeSystem();
+    }
+
+}
