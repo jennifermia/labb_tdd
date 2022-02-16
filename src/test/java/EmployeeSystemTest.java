78c20491f4c02e27921bb20c9007869d82d1019f
jennifermia <jennifer.toivanen@iths.se>
2022-02-16 14:28:00 +0100

    red

--- src/test/java/EmployeeSystemTest.java
+++ src/test/java/EmployeeSystemTest.java
@@ -7,26 +7,31 @@ import java.util.ArrayList;
 public class EmployeeSystemTest {
 
     EmployeeSystem testObject;
-    Employee tjomme;
+    Employee tjomme1;
+    Employee tjomme2;
+    Employee tjomme3;
 
 
     @BeforeEach
     public  void setUpInit(){
        testObject = new EmployeeSystem();
+       tjomme1 = new Employee("Gösta","Karlsson",34,34000);
+        tjomme2 = new Employee("Göran","Karlsson",23,23000);
+        tjomme3 = new Employee("Pelle","Karlsson",50,33000);
     }
     @Test
     public void test_AddEmployee(){
-        tjomme = new Employee();
-        testObject.addEmployee(tjomme);
+        tjomme1 = new Employee();
+        testObject.addEmployee(tjomme1);
         ArrayList<Employee> employeeList = testObject.getEmployeeList();
         assertTrue(employeeList.size() != 0);
     }
 
     @Test
     public void test_RemoveEmployee(){
-        tjomme = new Employee();
-        testObject.addEmployee(tjomme);
-        testObject.removeEmployee(tjomme);
+        tjomme1 = new Employee();
+        testObject.addEmployee(tjomme1);
+        testObject.removeEmployee(tjomme1);
         ArrayList<Employee> actualList = testObject.getEmployeeList();
         assertEquals(0,actualList.size());
         //assertTrue(actualList.size() == 0);
@@ -34,15 +39,25 @@ public class EmployeeSystemTest {
 
     @Test
     public void test_SpecificEmployeeInList(){
-        tjomme = new Employee("exempel","exempel",2,22222);
-        testObject.addEmployee(tjomme);
-        tjomme = new Employee("exempel","exempel",20,22222);
-        testObject.addEmployee(tjomme);
+        tjomme1 = new Employee("exempel","exempel",2,22222);
+        testObject.addEmployee(tjomme1);
+        tjomme1 = new Employee("exempel","exempel",20,22222);
+        testObject.addEmployee(tjomme1);
         ArrayList<Employee> EmployeeList = testObject.getEmployeeList();
         EmployeeList.forEach(employee -> System.out.println(employee.getAge()));
         assertEquals("exempel", EmployeeList.get(0).getFirstName());
+    }
+
+    @Test
+    public void test_Increase_OneSalary(){
 
     }
 
+    @Test
+    public void test_IncreaseAllSalaries(){
+    testObject.increaseAllSalaries();
+
+
+    }
 
 }
